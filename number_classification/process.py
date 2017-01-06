3b5a8caa705d2cf457e1376966fb3bf7d5f38bb7
David Ferris <david.j.ferris.21@gmail.com>
2017-01-05 23:24:54 -0500

    read binary from file into arr

--- number_classification/process.py
+++ number_classification/process.py
@@ -1,1 +1,11 @@
+# -*- coding: utf8 -*-
 # Processed MNIST dataset (http://cis.jhu.edu/~sachin/digit/digit.html)
+from array import array
+
+NUM_BYTES = 28*28*1000
+data = array('B')
+with open('data/data0', 'rb') as f:
+    data.fromfile(f, NUM_BYTES)
+
+for x in range(1,28):
+	print data[0:28*x]
