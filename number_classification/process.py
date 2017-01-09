e8ebe00336777748271912bbf6f7b8e0f9ab2aef
David Ferris <david.j.ferris.21@gmail.com>
2017-01-09 17:17:33 -0500

    properly iterate over #s

--- number_classification/process.py
+++ number_classification/process.py
@@ -2,10 +2,11 @@
 # Processed MNIST dataset (http://cis.jhu.edu/~sachin/digit/digit.html)
 from array import array
 
-NUM_BYTES = 28*28*1000
+NUM_BYTES = 28 * 28 * 1000
 data = array('B')
-with open('data/data0', 'rb') as f:
+with open('data/data7', 'rb') as f:
     data.fromfile(f, NUM_BYTES)
 
-for x in range(1,28):
-	print data[0:28*x]
+for x in range(1,28*5):
+	print data[28*(x-1):28*x]
+
