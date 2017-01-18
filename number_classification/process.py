30edf13438efa559d637f15b268e5bde4f20a625
David Ferris <david.j.ferris.21@gmail.com>
2017-01-18 16:22:24 -0500

    google demo code minus mnist package

--- number_classification/process.py
+++ number_classification/process.py
@@ -14,11 +14,28 @@ for i in range(0,10):
 	    temp.fromfile(f, IMAGE_BYTES * 1000)
 	    data.append(temp)
 
-# for x in range(1,28*5):
+# for x in range(1,28*5): 
 # 	print data[4][28*(x-1):28*x]
+
+print len(data[0])
 
 x = tf.placeholder(tf.float32, [None, IMAGE_BYTES])
 
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
 
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+
+
+# for i in range(1000):
+#   batch_xs, batch_ys = mnist.train.next_batch(100)
+#   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
