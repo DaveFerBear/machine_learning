7acd3eb2c33d375384940ac35326fbda4dee5e59
David Ferris <david.j.ferris.21@gmail.com>
2017-08-27 11:10:10 -0700

    AirHockey class init boilerplate

--- airhockey_ai/airhockey.py
+++ airhockey_ai/airhockey.py
@@ -1,7 +1,31 @@
+'''
+This class contains logic for air hockey physics engine
+'''
 class AirHockeySim(object):
-	def __init__(self):
-		pass
 
+	def __init__(self, player_radius=5, puck_radius=4, rink_dim=(100,200), net_width=20):
+		#Gameplay params
+		self._player_radius = player_radius
+		self._puck_radius = puck_radius
+		self._rink_dim = rink_dim
+		self._net_width = net_width
+
+		#Players
+		self._p1 = Player(rink_dim[0]/2., 10)
+		self._p2 = Player(rink_dim[0]/2., rink_dim[1]-10)
+		
 	def print_status(self):
 		print('working')
 
+
+
+class Player(object):
+	def __init__(self, x, y, vx=0, vy=0):
+		self._x = x
+		self._y = y
+		self._vx = vx
+		self._vy = vy
+
+	def update():
+		self._x += self._vx
+		self._y += self._vy
--- airhockey_ai/test.py
+++ airhockey_ai/test.py
@@ -1,23 +1,24 @@
 from airhockey import AirHockeySim
+try:
+    import Tkinter as tk # this is for python2
+except:
+    import tkinter as tk # this is for python3
 
 def test_gui():
-	try:
-	    import Tkinter as tk # this is for python2
-	except:
-	    import tkinter as tk # this is for python3
-
-	root=tk.Tk()
-	canvas=tk.Canvas(root,width=400,height=400)
+	root = tk.Tk()
+	canvas = tk.Canvas(root,width=400,height=400)
 	canvas.pack()
-	circle=canvas.create_oval(50,50,80,80,outline="white",fill="blue")
+	circle = canvas.create_oval(50,50,80,80,outline="white",fill="blue")
 
 	def redraw():
 	   canvas.after(100,redraw)
-	   canvas.move(circle,5,5)
+	   canvas.move(circle,2,2)
+
 	canvas.after(100,redraw)
 	root.mainloop()
 
-
+def test_airhockey():
+	hockey = AirHockeySim()
 
 if __name__ == '__main__':
 	ah = AirHockeySim()
