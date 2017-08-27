ceefd0926bed3a58362f614c7fcf86b22510b6fb
David Ferris <david.j.ferris.21@gmail.com>
2017-08-27 12:43:51 -0700

    drawing canvas and all players with appropriate shift param

--- airhockey_ai/airhockey.py
+++ airhockey_ai/airhockey.py
@@ -3,29 +3,37 @@ This class contains logic for air hockey physics engine
 '''
 class AirHockeySim(object):
 
-	def __init__(self, player_radius=5, puck_radius=4, rink_dim=(100,200), net_width=20):
-		#Gameplay params
+	def __init__(self, player_radius=12, puck_radius=8, rink_dim=(200,400), net_width=20):
+		#Gameplay Params
 		self._player_radius = player_radius
 		self._puck_radius = puck_radius
 		self._rink_dim = rink_dim
 		self._net_width = net_width
 
 		#Players
-		self._p1 = Player(rink_dim[0]/2., 10)
-		self._p2 = Player(rink_dim[0]/2., rink_dim[1]-10)
-		
+		self._p1 = Player(rink_dim[0]/2., 100, player_radius)
+		self._p2 = Player(rink_dim[0]/2., rink_dim[1]-100, player_radius)
+
+		self._puck = Player(rink_dim[0]/2., rink_dim[1]/2., puck_radius)
+
 	def print_status(self):
 		print('working')
 
-
-
 class Player(object):
-	def __init__(self, x, y, vx=0, vy=0):
+	def __init__(self, x, y, radius, vx=0, vy=0):
 		self._x = x
 		self._y = y
+		self._r = radius
 		self._vx = vx
 		self._vy = vy
 
-	def update():
+	def update(self):
 		self._x += self._vx
 		self._y += self._vy
+
+	def get_corner_pos(self, shift=0): #rendering helper
+		x0 = self._x - self._r + shift
+		y0 = self._y - self._r + shift
+		x1 = self._x + self._r + shift
+		y1 = self._y + self._r + shift
+		return x0, y0, x1, y1
--- airhockey_ai/test.py
+++ airhockey_ai/test.py
@@ -1,25 +1,47 @@
 from airhockey import AirHockeySim
 try:
-    import Tkinter as tk # this is for python2
+    import Tkinter as tk # Python 2
 except:
-    import tkinter as tk # this is for python3
+    import tkinter as tk # Python 3
 
+'''
+Used to verify tkinter works properly
+'''
 def test_gui():
 	root = tk.Tk()
 	canvas = tk.Canvas(root,width=400,height=400)
 	canvas.pack()
 	circle = canvas.create_oval(50,50,80,80,outline="white",fill="blue")
-
 	def redraw():
 	   canvas.after(100,redraw)
 	   canvas.move(circle,2,2)
-
 	canvas.after(100,redraw)
 	root.mainloop()
 
+
+'''
+Used to verify physics engine working properly
+'''
 def test_airhockey():
-	hockey = AirHockeySim()
+	sim = AirHockeySim()
+
+	# Canvas Boilerplate
+	width, height = sim._rink_dim
+	BORDER = 25
+	root = tk.Tk()
+	canvas = tk.Canvas(root, width=width+2*BORDER, height=height+2*BORDER)
+	canvas.pack()
+
+	# Draw Rink
+	rect = canvas.create_rectangle(BORDER, BORDER, width+BORDER, height+BORDER)
+
+	#Draw Puck and Players
+	p1 = canvas.create_oval(sim._p1.get_corner_pos(shift=BORDER))
+	p2 = canvas.create_oval(sim._p2.get_corner_pos(shift=BORDER))
+	puck = canvas.create_oval(sim._puck.get_corner_pos(shift=BORDER))
+
+	root.mainloop()
 
 if __name__ == '__main__':
 	ah = AirHockeySim()
-	test_gui()
+	test_airhockey()
